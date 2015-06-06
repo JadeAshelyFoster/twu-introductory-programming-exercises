6f1333c41ecc19719f035ae4b97939e2934c8228
JadeAshleyFoster <jadefoster29@gmail.com>
2015-06-06 14:58:24 +0100

    Written test to draw vertical asterisk, but no functionality yet

--- src/main/java/TrianglePrinter.java
+++ src/main/java/TrianglePrinter.java
@@ -1,4 +1,22 @@
 
+public class TrianglePrinter {
 
+    public static void main(String[] args) {
+        TrianglePrinter tPrinter = new TrianglePrinter();
+        System.out.println(tPrinter.getAsterisk());
+        System.out.println(tPrinter.getAsterisksHorizontal(8));
+    }
+
+    public String getAsterisk() {
+        return "*";
+    }
+
+    public String getAsterisksHorizontal(int n) {
+        String asterisks = "";
+        for (int i =0; i < n; i++) {
+            asterisks += getAsterisk();
+        }
+        return asterisks;
+    }
 
 }
--- src/test/java/TrianglePrinterTest.java
+++ src/test/java/TrianglePrinterTest.java
@@ -1,1 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class TrianglePrinterTest {
+    TrianglePrinter testTrianglePrinter;
+
+    @Before
+    public void setUp() {
+        testTrianglePrinter = new TrianglePrinter();
+    }
+
+    @Test
+    public void testGetAsterisk() {
+        String output = testTrianglePrinter.getAsterisk();
+        assertEquals("*", output);
+    }
+
+    @Test
+    public void testGetHorixontalLineOfNAsterisks() {
+        String output = testTrianglePrinter.getAsterisksHorizontal(8);
+        assertEquals("********", output);
+        String output2 = testTrianglePrinter.getAsterisksHorizontal(3);
+        assertEquals("***", output2);
+    }
+
+    @Test
+    public void testGetHorizontalLineOfNAsterisks() {
+        String output = testTrianglePrinter.getAsteriskVertical(8);
+        assertEquals("*\n*\n*\n*\n*\n*\n*\n*", output);
+        String output2 = testTrianglePrinter.getAsteriskVertical(3);
+        assertEquals("*\n*\n*\n", output2);
+    }
+}
